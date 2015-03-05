ae36e87b118840771e22d6cb59c9e5f9c730be35
tim2611 <tim2611@gmail.com>
2015-03-05 08:42:51 +0100

    implemented spinbox for bits (key generation openssl)

--- csr.py
+++ csr.py
@@ -1,10 +1,10 @@
 #small python program to create an csr for a sll certificate
 
-#ToDo add a listfield to change the bits for the request 1024 2048 4096
 
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import Spinbox
 from os.path import expanduser
 import os
 
@@ -22,8 +22,9 @@ def generate_key(*args):
     try:
         fqdn = certname.get()
         path = tkfd.get() + "/"
+        bits = spbbits.get()
         tkfd.set(path)
-        os.system("xterm -e openssl genrsa -out " + path + fqdn + ".key 4096")
+        os.system("xterm -e openssl genrsa -out " + path + fqdn + ".key " +bits)
         btnGenerate.config(state='enabled')
     except ValueError:
         pass
@@ -48,15 +49,17 @@ tkfd = StringVar()
 home = expanduser("~")
 
 certname_entry = ttk.Entry(mainframe, width=7, textvariable=certname)
-certname_entry.grid(column=2, row=1, sticky=(W, E))
+certname_entry.grid(column=2, row=2, sticky=(W, E))
 
 ttk.Button(mainframe, text="Generate Key", command=generate_key).grid(column=1, row=3, sticky=W)
-ttk.Button(mainframe, text="Set Dir", command=dir).grid(column=3, row=1, sticky=W)
+ttk.Button(mainframe, text="Set Dir", command=dir).grid(column=1, row=1, sticky=W)
 btnGenerate = ttk.Button(mainframe, text="Generate CSR", state='disabled', command=generate_csr)
 btnGenerate.grid(column=2, row=3, sticky=W)
+spbbits = Spinbox(mainframe, values=(4096, 2048, 1024))
+spbbits.grid(column=3, row=2, sticky=E)
 ttk.Button(mainframe, text="Cancel", command=exit).grid(column=3, row=3, sticky=W)
 
-ttk.Label(mainframe, text="FQDN Name:").grid(column=1, row=1, sticky=E)
+ttk.Label(mainframe, text="FQDN Name:").grid(column=1, row=2, sticky=E)
 
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
